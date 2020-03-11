ba0146d530d413a610928e917cba7246bfcabc0e
Xuzheng <linxz@garena.com>
2020-03-11 21:14:32 +0800

    UPD: 2nd review for Vue Matery intro 1-8

--- Vue/test_0220/vuemastery/lxz_single_html/main.js
+++ Vue/test_0220/vuemastery/lxz_single_html/main.js
@@ -1,132 +1,95 @@
-Vue.component ('product', {
-  template: `
-  <div class="product">
-    <div class="product-image">
-      <img v-bind:src="image">
-    </div>
-    <div class="product-info">
-      <h1>{{title}}</h1>
-      <h3>Here are some products: {{product}}</h3>
-      <a v-bind:href="link">a link</a>
-      <p v-show="onSale">On Sale!</p>
-      <ul>
-        <li v-for="item in details">{{item}}</li>
-      </ul>
-      <div v-for="(vari, index) in variants" 
-          v-bind:key="vari.variantId"
-          class = "color-box"
-          v-bind:style="{backgroundColor: vari.variantColor }"
-          v-on:mouseOver="updateProduct(index)"
-          >
+Vue.component('product', {
+  props: {
+    userpremium: {
+      required: true,
+      type: Boolean
+    }
+  },
+  template:`
+    <div>
+      <div class="product">
+        <div class="product-image">
+          <img :src="image" :alt="product">
+        </div>
+        <div class="product-info">
+          <h2>{{ product }}</h2>
+          <p v-if="inventory>20">In Stock</p>
+          <p v-else-if="inventory>0">Almost sold out</p>
+          <p v-else>Out of Stock</p>
+          <ul>
+            <li v-for="(property, index) in properties" :key="index">{{ property }}</li>
+          </ul>
+          <div 
+            v-for="(variant, index) in variants" 
+            :key="variant.variantID"
+            @mouseover="updateProduct(index)"
+            class="color-box"
+            :style="{backgroundColor: variant.variantColor}"
+            >
+            
+          </div>        
+        </div>      
       </div>
-      <button v-on:click="addToCart"       
-              v-bind:class="{disabledButton: !inStock}"
-              v-bind:disabled=!inStock>Add to Cart</button>
-      <button v-on:click="removeFromCart" v-if="cart > 0">Remove from Cart</button>
+      <button 
+        class="button" 
+        @click="updateCart"
+        :disabled="inventory==0"
+        :class="{disabledButton: inventory==0}"
+        >Add to Cart</button>
       <div class="cart">
-        <p>Cart: ({{cart}})</p>
+        <p>Cart {{ cart }}</p>
       </div>
+      <p>User is premium: {{userpremium}}</p>
     </div>
-  </div>  
   `,
   data() {
-    return{
-    brand: 'Qing Luan',
-    product: 'Swords',
-    selectedVariant: 0,
-    link: '####',
-    inventory: 10,
-    onSale: true,
-    details: ["80% cotton", "20% polyester"],
-    variants: [
-      {
-        variantId: 1234,
-        variantColor: "green",
-        variantImage: './assets/vmSocks-green-onWhite.jpg',
-        variantQuantity: 10
-      },
-      {
-        variantId: 1235,
-        variantColor: "blue",
-        variantImage: './assets/vmSocks-blue-onWhite.jpg',
-        variantQuantity: 0
-      }
-    ],
-    cart: 0
+    return {
+      product: 'Water',
+      // image: './assets/vmSocks-green-onWhite.jpg',
+      // inStock: true,
+      // inventory: 8,
+      properties: ['Vue Mastery Full Course', 'Vue crash with local Vue and component file', 'AWS', 'Portfolio site on cloud'],
+      variants: [
+        {
+          variantID: 1234,
+          variantColor: 'green',
+          variantImage: './assets/vmSocks-green-onWhite.jpg',
+          variantInventory: 100
+        },
+        {
+          variantID: 1235,
+          variantColor: 'blue',
+          variantImage: './assets/vmSocks-blue-onWhite.jpg',
+          variantInventory: 0
+        }
+      ],
+      selectedVariant: 0,
+      cart: 0
     }
   },
   methods: {
-    addToCart: function() {
-      this.cart += 1
+    updateCart() {
+      this.cart += 1;
+      this.variants[this.selectedVariant].variantInventory -= 1;
     },
-    removeFromCart: function() {
-      this.cart -= 1
-    },
-    updateProduct: function(index) {
-      console.log(index)
-      this.selectedVariant = index
+    updateProduct(index) {
+      this.selectedVariant = index;
     }
   },
   computed: {
-    title: function() {
-      return this.brand + ' ' + this.product
-    },
-    image: function() {
+    image() {
       return this.variants[this.selectedVariant].variantImage
     },
-    inStock: function() {
-      return this.variants[this.selectedVariant].variantQuantity
+    inventory() {
+      return this.variants[this.selectedVariant].variantInventory
     }
   }
 })
 
-var app = new Vue({
+
+var app = new Vue ({
   el: '#app',
   data: {
-    brand: 'Qing Luan',
-    product: 'Swords',
-    selectedVariant: 0,
-    link: '####',
-    inventory: 10,
-    onSale: true,
-    details: ["80% cotton", "20% polyester"],
-    variants: [
-      {
-        variantId: 1234,
-        variantColor: "green",
-        variantImage: './assets/vmSocks-green-onWhite.jpg',
-        variantQuantity: 10
-      },
-      {
-        variantId: 1235,
-        variantColor: "blue",
-        variantImage: './assets/vmSocks-blue-onWhite.jpg',
-        variantQuantity: 0
-      }
-    ],
-    cart: 0
-  },
-  methods: {
-    addToCart: function() {
-      this.cart += 1
-    },
-    removeFromCart: function() {
-      this.cart -= 1
-    },
-    updateProduct: function(index) {
-      console.log(index)
-      this.selectedVariant = index
-    }
-  },
-  computed: {
-    title: function() {
-      return this.brand + ' ' + this.product
-    },
-    image: function() {
-      return this.variants[this.selectedVariant].variantImage
-    },
-    inStock: function() {
-      return this.variants[this.selectedVariant].variantQuantity
-    }
+    premium: true
   }
 })
